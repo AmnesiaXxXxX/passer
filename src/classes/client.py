9a42320e83c53081e0de326aa0a07856202fd3c4
amnesiawho <92972232+AmnesiaXxXxX@users.noreply.github.com>
2025-04-11 07:46:41 +0000

    Refactor CustomClient to enhance argument checking and setup message handlers

--- src/classes/client.py
+++ src/classes/client.py
@@ -2,17 +2,18 @@
 
 import datetime
 import logging
-from typing import Any, Optional, Coroutine
-
+from typing import Any, Callable, List, Optional, Coroutine, Self
+import inspect
 from pyrogram.client import Client
-
+from pyrogram.handlers.message_handler import MessageHandler
+from pyrogram import filters
 
 class CustomClient(Client):
     """Кастомный класс клиента"""
 
     def __init__(
         self,
-        name: str | None = "bot",
+        name: Optional[str] = "bot",
         api_id: Optional[str | int] = None,
         api_hash: Optional[str] = None,
         bot_token: Optional[str] = None,
@@ -20,10 +21,11 @@ class CustomClient(Client):
         self.logger = logging.getLogger("pyrobot")
         if not self.check_args(api_id, api_hash, name, bot_token):
             return
+
         logging.info("Проверка аргументов прошла успешно")
         super().__init__(name, api_id, api_hash)
 
-    def check_args(self, api_id, api_hash, name, bot_token):
+    def check_args(self, api_id: Optional[str | int], api_hash: Optional[str], name: Optional[str], bot_token: Optional[str]) -> bool:
         """Проверка аргументов"""
         if (api_id is None) or (api_hash is None):
             raise ValueError(
@@ -54,7 +56,24 @@ class CustomClient(Client):
             )
         return True
 
+    def setup_handlers(self):
+        """Настройка хендлеров отслеживания сообщений"""
+        super_functions = [name for name, _ in super().__class__.__dict__.items()]
+        async_functions: List[Callable[..., Any]] = []
+        for name, func in self.__class__.__dict__.items():
+            if inspect.iscoroutinefunction(func) and name not in super_functions:
+                if name.startswith("handle_"):
+                    
+                    self.add_handler(MessageHandler(func, filters.command(name[8:].split("_"))))
+                async_functions.append(func)
+                
+        self.logger.info(
+            "Найдено асинхронных функций: %s",
+            len([func.__name__ for func in async_functions]),
+        )
+
     def run(self, coroutine: Optional[Coroutine[Any, Any, Any]] = None) -> None:
         """Запуск клиента"""
         self.logger.info(f"Старт бота в {datetime.datetime.now(datetime.UTC)}")
+        self.setup_handlers()
         super().run(coroutine)
